fcf2a5a1fe94a539122075ed3659a9c2e65e262f
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-12-06 10:18:54 +0800

    ariadi

--- admin/tes.php
+++ admin/tes.php
@@ -7,6 +7,17 @@ $tampil = mysqli_query($db, $hasil);
 
 ?>
 
+<!-- datatable -->
+<!-- <link href="twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet" />
+<link href="1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+<script  src="https://code.jquery.com/jquery-3.7.0.js"> </script>
+<script  src="1.13.7/js/jquery.dataTables.min.js"> </script>
+<script  src="1.13.7/js/dataTables.bootstrap5.min.js"> </script>
+<script  src="../assets/js/datatables.js"> </script> -->
+
+<link href="../assets/DataTables/DataTables-1.13.8/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+<link href="../assets/DataTables/Buttons-2.4.2/css/buttons.bootstrap5.min.css " rel="stylesheet" />
+
 <body class="g-sidenav-show bg-gray-200">
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
         <div class="container-fluid py-4">
@@ -19,24 +30,23 @@ $tampil = mysqli_query($db, $hasil);
                             </div>
                         </div>
                         <div class="card-body px-0 pb-2">
-                            <div class="table-responsive p-0">
-                                <table class="table align-items-center mb-0">
+                            <div class="table-responsive p-3">
+                                <table id="example" class="table table-striped" style="width:100%">
                                     <thead>
                                         <tr>
-                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder ">Idpel</th>
-                                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder  ps-2">Nama</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Daya</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Tipe</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Lokasi</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Pmet</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Merek</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">tipe meter</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Nomer Meter</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Keterangan</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Rincian</th>
-                                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Opsi</th>
-                                            <th class="text-secondary "></th>
+                                            <th>Idpel</th>
+                                            <th>Nama</th>
+                                            <th>Daya</th>
+                                            <th>Tipe</th>
+                                            <th>Lokasi</th>
+                                            <th>Pmet</th>
+                                            <th>Merek</th>
+                                            <th>tipe meter</th>
+                                            <th>Nomer Meter</th>
+                                            <th>Keterangan</th>
+                                            <th>Rincian</th>
                                         </tr>
+                                    </thead>
                                     </thead>
                                     <tbody>
                                         <?php
@@ -69,16 +79,27 @@ $tampil = mysqli_query($db, $hasil);
                                                         <?php echo $d['ket2'] ?>
                                                     </div>
                                                 </td>
-                                                <!-- <td class="text-center">
-                                                    <a href="pelangganaksi.php?kode=<?php echo $d['idpel'] ?>&aksi=ubah" class="btn btn-success">Ubah</a>
-                                                    <a href="javascript:void(0);" class="btn btn-danger" onclick="hapusData('<?php echo $d['idpel']; ?>')">Hapus</a>
-                                                </td> -->
                                             </tr>
                                         <?php
                                             $counter++;
                                         }
                                         ?>
                                     </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Idpel</th>
+                                            <th>Nama</th>
+                                            <th>Daya</th>
+                                            <th>Tipe</th>
+                                            <th>Lokasi</th>
+                                            <th>Pmet</th>
+                                            <th>Merek</th>
+                                            <th>tipe meter</th>
+                                            <th>Nomer Meter</th>
+                                            <th>Keterangan</th>
+                                            <th>Rincian</th>
+                                        </tr>
+                                    </tfoot>
                                 </table>
                             </div>
                         </div>
@@ -90,12 +111,50 @@ $tampil = mysqli_query($db, $hasil);
     <?php
     include '../assets/layouts/setting.php'
     ?>
+
+    <!-- Datatable download -->
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="../assets/DataTables/DataTables-1.13.8/js/jquery.dataTables.min.js"> </script>
+    <script src="../assets/DataTables/DataTables-1.13.8/js/dataTables.bootstrap5.min.js"> </script>
+    <script src="../assets/DataTables/Buttons-2.4.2/js/dataTables.buttons.js"> </script>
+    <script src="../assets/DataTables/Buttons-2.4.2/js/buttons.bootstrap5.min.js"> </script>
+    <script src="../assets/DataTables/JSZip-3.10.1/jszip.min.js"> </script>
+    <script src="../assets/DataTables/pdfmake-0.2.7/pdfmake.min.js"> </script>
+    <script src="../assets/DataTables/pdfmake-0.2.7/vfs_fonts.js"> </script>
+    <script src="../assets/DataTables/Buttons-2.4.2/js/buttons.html5.min.js"> </script>
+    <script src="../assets/DataTables/Buttons-2.4.2/js/buttons.print.min.js"> </script>
+    <script src="../assets/DataTables/Buttons-2.4.2/js/buttons.colVis.min.js"> </script>
+
+
+    <!-- 
+    https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js
+    https://cdn.datatables.net/buttons/2.4.2/js/buttons.bootstrap5.min.js
+    https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js
+    https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js
+    https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js
+    https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js
+    https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js
+    https://cdn.datatables.net/buttons/2.4.2/js/buttons.colVis.min.js -->
+
+
+    <script>
+        $(document).ready(function() {
+            var table = $('#example').DataTable({
+                lengthChange: false,
+                buttons: ['copy', 'excel', 'pdf', 'colvis']
+            });
+
+            table.buttons().container()
+                .appendTo('#example_wrapper .col-md-6:eq(0)');
+        });
+    </script>
+
     <!--   Core JS Files   -->
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
     <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script async src="https://buttons.github.io/buttons.js"></script>
     <script src="../assets/js/material-dashboard.min.js?v=3.1.0"></script>
     <script>
         var win = navigator.platform.indexOf("Win") > -1;
