e4e4ee65cf0ae6d20d60b15b3259d6e4ba6011cc
c030321097@mahasiswa.poliban.ca.id <c030321097@mahasiswa.poliban.ac.id>
2023-12-07 14:03:07 +0800

    ariadi

--- admin/pelangganinput.php
+++ admin/pelangganinput.php
@@ -83,7 +83,7 @@ $tampil = mysqli_query($db, $hasil);
             <div class="row">
                 <div class="col-12">
                     <div class="card my-4">
-                        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                        <div class="card-header p-0 position-relative mt-n4 mx-3">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
                                 <h6 class="text-white text-capitalize ps-3">Pelanggan</h6>
                             </div>
@@ -144,7 +144,7 @@ $tampil = mysqli_query($db, $hasil);
 
                                                 <td class="text-center">
                                                     <a href="javascript:void(0);" onclick="tampilkanGambar('../assets/file/datpel/<?php echo $d['pmet']; ?>')">
-                                                        <img src="../assets/file/datpel/<?php echo $d['pmet']; ?>" style="width: 50px; height: 100px">
+                                                        <img src="../assets/img/datpel/<?php echo $d['pmet']; ?>" style="width: 50px; height: 100px">
                                                     </a>
                                                 </td>
 
